f41c6b5772097e8b9b1732c22e7f6719f1ab4fca
Anthone-Tapia <antovero95@gmail.com>
2023-05-24 17:39:57 +0200

    Se realizan validaciones de campos

--- scripts/main.js
+++ scripts/main.js
@@ -4,38 +4,66 @@ const categoria = document.getElementById('category');
 const ticketValue = 200;
 
 btnCleaner.addEventListener('click', limpiar);
-btnResumen.addEventListener('click', calcular);
+btnResumen.addEventListener('click', validarCampos);
 
-function limpiar(){
+function limpiar() {
     document.getElementById('form-ticket').reset();
     document.getElementById('detalle').innerHTML = '';
     document.getElementById('detalle').style.display = 'none';
 
 }
 
-function calcular(){
-    let cantidad = document.getElementById('cant').value;
+function validarCampos() {
+    let name = document.getElementById('name').value;
+    let lastname = document.getElementById('lastName').value;
+    let email = document.getElementById('email').value;
+    let cant = document.getElementById('cant').value;
+
+    if (name === '' || lastname === '' || email === '' || cant === '') {
+        alert("Por favor completa todos los campos.");
+        return;
+    } else if (name.length < 3 || lastname.length < 3) {
+        alert('El nombre y el apellido deben tener al menos 3 caracteres.');
+        return;
+    }
+
+    if (!validarCorreo(email)) {
+        alert('El correo electrónico no tiene un formato válido.');
+        return;
+    }
+
+    calcular();
+}
+
+function validarCorreo(correo) {
+    var regexp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regexp.test(correo);
+}
+
+function calcular() {
+    let cantidad = parseInt(document.getElementById('cant').value);
     let categoriaSeleccionada = categoria.value;
     let descuento = 0;
     let total = cantidad * 200;
 
-   
-    if(total > 0 && categoriaSeleccionada == 'Estudiante'){
+    if (total > 0 && categoriaSeleccionada == 'Estudiante') {
         descuento = total * 0.8;
         total -= descuento;
-    } else if (total > 0 && categoriaSeleccionada == 'Trainee'){
+    } else if (total > 0 && categoriaSeleccionada == 'Trainee') {
         descuento = total * 0.5;
         total -= descuento;
-    } else if (total > 0 && categoriaSeleccionada == 'Junior'){
+    } else if (total > 0 && categoriaSeleccionada == 'Junior') {
         descuento = total * 0.15;
         total -= descuento;
     }
 
 
-    if (total <= 0){
+    if (total <= 0) {
+        document.getElementById('detalle').style.display = 'block';
         document.getElementById('detalle').innerHTML = `Debe ingresar una cantidad mayor a 0`;
     } else {
         document.getElementById('detalle').style.display = 'block';
         document.getElementById('detalle').innerHTML = `Total a Pagar: $${total}`;
     }
 }
+
